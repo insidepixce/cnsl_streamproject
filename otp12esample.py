68ae81973b3bfbf93a7dc7d6d3e0638db66437bf
insidepixce <insidepixce@gmail.com>
2023-05-27 15:32:09 +0900

    소숫점 초까지 확인/완성

--- otp12esample.py
+++ otp12esample.py
@@ -11,8 +11,8 @@ class LFSR:
         return self.state.pop(0)
 
 def lfsr_otp(length):
-    seed = int(time.time())  
-    seed = [int(i) for i in bin(seed)[2:].zfill(16)]  
+    seed = int(time.time() * 1000)  #밀리세컨드까지 사용
+    seed = [int(i) for i in bin(seed)[-16:].zfill(16)]  
     lfsr = LFSR(seed, [15, 13, 12, 10])  
     return ''.join(str(lfsr.step()) for _ in range(length))
 
