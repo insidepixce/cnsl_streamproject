bbaca83e09900a29cff442ce5075b9d788ff09c0
insidepixce <insidepixce@gmail.com>
2023-05-27 15:20:22 +0900

    시드 값이 그대로 나오지는 않지만 한 가지 otP만 계속 나옴

--- otp12esample.py
+++ otp12esample.py
@@ -1,8 +1,9 @@
 def lfsr(seed, taps):
     while True:
-        feedback = sum(seed[i] for i in taps) % 2
-        seed = [feedback] + seed[:-1]
-        yield seed[-1]
+        feedback = seed[-1]  # 마지막 비트를 피드백 비트로 사용
+        new_bit = sum(seed[i] for i in taps) % 2  # 터치 위치의 비트들의 합을 계산
+        seed = [new_bit] + seed[:-1]  # 시드 값 업데이트
+        yield feedback
 
 def generate_otp(seed, taps, length):
     otp = []
