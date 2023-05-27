7afc044dea3546e03fea65bd85a04137715ad819
insidepixce <insidepixce@gmail.com>
2023-05-27 15:22:09 +0900

    시드 값 업데이트

--- otp12esample.py
+++ otp12esample.py
@@ -1,15 +1,16 @@
 def lfsr(seed, taps):
     while True:
-        feedback = seed[-1]  # 마지막 비트를 피드백 비트로 사용
+        feedback = seed[0]  # 첫 번째 비트를 피드백 비트로 사용
         new_bit = sum(seed[i] for i in taps) % 2  # 터치 위치의 비트들의 합을 계산
-        seed = [new_bit] + seed[:-1]  # 시드 값 업데이트
-        yield feedback
+        seed = seed[1:] + [new_bit]  # 시드 값 업데이트
+        yield feedback, seed
 
 def generate_otp(seed, taps, length):
     otp = []
     generator = lfsr(seed, taps)
     for _ in range(length):
-        otp.append(str(next(generator)))
+        feedback, seed = next(generator)
+        otp.append(str(feedback))
     return ''.join(otp)
 
 seed = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  # 초기 시드 값
